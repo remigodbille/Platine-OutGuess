f4456309fd9fb04ad10417cccda15bec0b54bf37
Chris <christo5993@gmail.com>
2015-12-02 17:23:28 +0100

    projet android vide

---
+++ app/src/main/java/com/fil/platine/outguess/QuestionActivity.java
@@ -0,0 +1,14 @@
+package com.fil.platine.outguess;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class QuestionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+
+}
