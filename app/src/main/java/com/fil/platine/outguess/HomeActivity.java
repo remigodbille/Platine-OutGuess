c2b339e03a2ef4a93580330033631320ed3e9871
Chris <christo5993@gmail.com>
2016-01-05 17:34:43 +0100

    Ebauche activité QuestionActivity.java

--- app/src/main/java/com/fil/platine/outguess/HomeActivity.java
+++ app/src/main/java/com/fil/platine/outguess/HomeActivity.java
@@ -1,14 +1,30 @@
 package com.fil.platine.outguess;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
+
+    Button buttonPlay;
+    Button buttonMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        buttonPlay = (Button)findViewById(R.id.button_play);
+        buttonMedia = (Button)findViewById(R.id.button_media);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), QuestionActivity.class));
+            }
+        });
     }
 
 }
--- app/src/main/java/com/fil/platine/outguess/QuestionActivity.java
+++ app/src/main/java/com/fil/platine/outguess/QuestionActivity.java
@@ -1,14 +1,104 @@
 package com.fil.platine.outguess;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class QuestionActivity extends Activity {
+
+    ProgressBar mProgressBar;
+    CountDownTimer mCountDownTimer;
+
+    int mCurrProgress = 0;
+    int TIMER_QUESTION = 30;
+    String CORRECT_ANSWER = "test";
+
+    TextView[] mTextViewList;
+    int mNextItem;
+
+    EditText mEditTextAnswer;
+
+    Button mButtonValidate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setMax(TIMER_QUESTION);
+        mProgressBar.setProgress(mCurrProgress);
+
+        mTextViewList = new TextView[6];
+        mTextViewList[0] = (TextView)findViewById(R.id.textView1);
+        mTextViewList[1] = (TextView)findViewById(R.id.textView2);
+        mTextViewList[2] = (TextView)findViewById(R.id.textView3);
+        mTextViewList[3] = (TextView)findViewById(R.id.textView4);
+        mTextViewList[4] = (TextView)findViewById(R.id.textView5);
+        mTextViewList[5] = (TextView)findViewById(R.id.textView6);
+
+        mNextItem = 0;
+        mTextViewList[mNextItem].setTextColor(Color.BLACK);
+        mNextItem++;
+
+        mEditTextAnswer = (EditText)findViewById(R.id.textAnswer);
+
+        mButtonValidate = (Button)findViewById(R.id.button_validate);
+
+        mCountDownTimer = new CountDownTimer(30000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.i("TAG_Timer", "Tick of Progress : " + mCurrProgress + " " +millisUntilFinished);
+                mCurrProgress++;
+                mProgressBar.setProgress(mCurrProgress);
+
+                if(mCurrProgress%5 == 0) {
+                    Log.i("TAG_TIMER", "New Word");
+
+                    mTextViewList[mNextItem].setTextColor(Color.BLACK);
+                    mNextItem++;
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                Log.i("TAG_Timer", "Tick of Progress : " + mCurrProgress);
+                mCurrProgress++;
+                mProgressBar.setProgress(mCurrProgress);
+
+                Log.i("TAG_Timer", "Timer finished !");
+            }
+        };
+        mCountDownTimer.start();
+
+        mButtonValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(QuestionActivity.this);
+                dialog.setTitle("Résultat");
+                dialog.setMessage("Attente d'une réponse");
+                dialog.setCancelable(true);
+
+                if(checkAnswer(mEditTextAnswer.getText().toString())) {
+                    dialog.setMessage("BRAVO !");
+                } else dialog.setMessage("PERDU !");
+
+                dialog.show();
+            }
+        });
+    }
+
+    public boolean checkAnswer(String answer){
+        return answer.equals(CORRECT_ANSWER);
     }
 
 }
